1ae1097a7af5b5bd143e07ea9572f1f46dc201f9
NadyaCI <nismail65@smith.edu>
2023-09-21 00:27:42 -0400

    message here ya go

--- computer.py
+++ computer.py
@@ -1,10 +1,29 @@
 class Computer:
 
     # What attributes will it need?
-
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = 0
+    memory: int = 0
+    operating_system: str = ""
+    year_made: int = 0 
+    price: int = 0
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, description: str, processor_type: str, 
+                 hard_drive_capacity: int,  memory: int, 
+                 operating_system: str, year_made: int, price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
+        
     # What methods will you need?
+    def update_price(self, new_price: int):
+        self.price=new_price
+    
+    def update_os(self, new_os: str):
+        self.operating_system=new_os
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,42 @@
+from computer import Computer
+from typing import Dict, Union, Optional
+
 class ResaleShop:
-
     # What attributes will it need?
-
+    #inventory, itemID
+    inventory: Dict[int, Computer] = {}
+    itemID: str = 0
+    
     # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # Remember: in python, all constructors have the same name (__init__)    
+    def __init__(self, inventory: Dict, itemID: int):
+        self.inventory = inventory
+        self.itemID=itemID
 
-    # What methods will you need?
+    def buy(self, computer: Computer):
+        global itemID
+        self.itemID+=1
+        itemID=self.itemID
+        self.inventory[itemID]=computer
+        print("Item", itemID, "purchased!")
+
+    def sell(self, item: int):
+        del self.inventory[item]
+        print("Item", itemID, "sold!")
+
+    def list_inventory(self):
+        for item in self.inventory:
+            computer: str = ""
+            print(f'Item ID: {item} : {self.inventory[item]}')
+
+    #Buy, sell, store
+
+add: Computer = Computer("sh","th",1,4,"sh",2,3)
+print(add.price, add.operating_system)
+add.update_price(72)
+add.update_os("heehee")
+print(add.price, add.operating_system)
+
+shop=ResaleShop({},0)
+shop.buy(add)
+shop.list_inventory()
