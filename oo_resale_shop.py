04687ce18c353bcce7634c3cbae43c63b77b28aa
NadyaCI <nismail65@smith.edu>
2023-09-21 02:18:36 -0400

    updated final for A2

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -2,41 +2,98 @@ from computer import Computer
 from typing import Dict, Union, Optional
 
 class ResaleShop:
-    # What attributes will it need?
-    #inventory, itemID
+    # The attributes
     inventory: Dict[int, Computer] = {}
     itemID: str = 0
     
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)    
+    # The constructor    
     def __init__(self, inventory: Dict, itemID: int):
         self.inventory = inventory
-        self.itemID=itemID
+        self.itemID = itemID
 
+    # The methods
     def buy(self, computer: Computer):
         global itemID
-        self.itemID+=1
+        self.itemID += 1
         itemID=self.itemID
-        self.inventory[itemID]=computer
-        print("Item", itemID, "purchased!")
+        self.inventory[itemID] = computer
+        print("Item", itemID, "purchased!\n")
 
     def sell(self, item: int):
-        del self.inventory[item]
-        print("Item", itemID, "sold!")
+        if item in self.inventory:
+            del self.inventory[item]
+            print("Item", item, "sold!\n")
+        else:
+            print("Item", item, "not found. Please select another item to sell.\n")
 
     def list_inventory(self):
-        for item in self.inventory:
-            computer: str = ""
-            print(f'Item ID: {item} : {self.inventory[item]}')
+        if self.inventory:
+            for item in self.inventory:
+                computer=self.inventory[item]
+                print("Item ID", item, ": ", computer.description, ",", 
+                      computer.processor_type, "processor,", 
+                      computer.hard_drive_capacity, "hard drive capacity,", 
+                      computer.memory, "memory,",
+                      computer.operating_system, "operating system, made in",
+                      computer.year_made, ", price", computer.price)
+            print("All items listed.\n")
+        else:
+            print("No inventory to display.\n")
 
-    #Buy, sell, store
+    def refurbish(self, item: int, new_os :Optional[str] = None):
+        if item in self.inventory:
+            computer = self.inventory[item]
+            if int(computer.year_made) < 2000:
+                computer.price = 0 # too old to sell, donation only
+            elif int(computer.year_made) < 2012:
+                computer.price = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer.year_made) < 2018:
+                computer.price = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer.price = 1000 # recent stuff
+            if new_os!= None:
+                computer.update_os(new_os)
+                self.inventory[item] = computer
+            print("Item", item, "has been refurbished!\n")
 
-add: Computer = Computer("sh","th",1,4,"sh",2,3)
-print(add.price, add.operating_system)
-add.update_price(72)
-add.update_os("heehee")
-print(add.price, add.operating_system)
+        else:
+            print("Item", item, "not found. Please select another item to refurbish.\n")
+            
+def main():
+    shop: ResaleShop = ResaleShop({}, 0)
+    new_computer: Computer = Computer("Mac Pro (Late 2013)",
+                                      "3.5 GHc 6-Core Intel Xeon E5", 1024, 
+                                      64, "macOS Big Sur", 2013, 1500)
+    another_computer: Computer = Computer("Mac Pro (2020)", 
+                                          "2 GHz Quad-Core Intel Core i5",
+                                          512, 16, "macOS Ventura", 2020, 400)
+    print("-" * 23)
+    print("~COMPUTER RESALE STORE~")
+    print("-" * 23, "\n")
 
-shop=ResaleShop({},0)
-shop.buy(add)
-shop.list_inventory()
+    print("Buying", new_computer.description)
+    shop.buy(new_computer)
+
+    print("Buying", another_computer.description)
+    shop.buy(another_computer)
+
+    print("Checking inventory...")
+    shop.list_inventory()
+
+    new_OS: str = "macOS Ventura"
+    print("Refurbishing item ID 1, updating OS to", new_OS)
+    shop.refurbish(1, new_OS)
+
+    print("Checking inventory...")
+    shop.list_inventory()
+
+    print("Selling Item ID #")
+    shop.sell(3)
+
+    print("Selling Item ID 1")
+    shop.sell(1)
+
+    print("Checking inventory...")
+    shop.list_inventory()
+
+main()
--- reflection.md
+++ reflection.md
@@ -1,5 +1,5 @@
 Use this file to record your reflection on this assignment. 
-I started this too late and didn't finish it in time(I also though it was due a day later) so I couldn't get all the functionality in on the first go through.
+I started this too late and didn't finish it quite in time(I also though it was due a day later) so I couldn't get all the functionality in on the first go through. Once I finished it off though I don't think it was too difficult, I just had trouble initially understanding what I was supposed to do. The refurbish function tripped me up a bit because I didn't understand why it was included. I also got a bit confused about how to put it onto gradescope but I figured it out.
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
 Don't procrastinate so much, if you start it won't be as bad as you expect
